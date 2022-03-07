3237d8ed5a7903ac40479a023644aca523ad0be6
ko50 <kotyana1331@gmail.com>
2022-03-08 03:32:07 +0900

    [追加] フォント

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        zcool: ["ZCOOL QingKe HuangYou"],
+        dosis: ["Dosis"],
+        maru: ["Kosugi Maru"],
+      },
+      colors: {
+        'potato': "#473C56",
+      },
+    },
   },
   plugins: [],
 }
