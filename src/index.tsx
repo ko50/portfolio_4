458d85f17302099de567065b2ed462ecde2c42cd
ko50 <kotyana1331@gmail.com>
2022-03-08 02:40:26 +0900

    [追加] tailwindをセットアップ

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './styles/index.css';
+import './styles/tailwind.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
