3867597196c86246ca5314393360fc313fbb01be
ko50 <kotyana1331@gmail.com>
2022-03-08 03:40:02 +0900

    [追加] ヘッダー(仮)

---
+++ src/components/common/header/header.tsx
@@ -0,0 +1,5 @@
+export function Header() {
+    return <div className="bg-potato w-100 p-4 text-center shadow-md">
+        <div className="text-3xl text-white font-zcool">ko50.dev</div>
+    </div>;
+}
