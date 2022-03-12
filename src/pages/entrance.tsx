affd2eeb66406c5142204a1b71baf38482a1c42b
ko50 <kotyana1331@gmail.com>
2022-03-12 10:04:41 +0900

    [追加] ローディングアニメーション(仮)

---
+++ src/pages/entrance.tsx
@@ -0,0 +1,7 @@
+import { EntranceShade } from "../components/entrance/shade";
+
+export function Entrance() {
+    return <div className="absolute top-0 left-0 z-50">
+        <EntranceShade />
+    </div>;
+}
