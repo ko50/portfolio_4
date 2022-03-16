512abca085ebc64e8f4d9c6acb0b15dd0d7c8b99
ko50 <kotyana1331@gmail.com>
2022-03-16 19:29:58 +0900

    [修正] エラーがうるさいのでkey渡した

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { routeList } from "config/routes";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        {routeList.map((v) => <Route exact path={v.path} component={v.page} />)}
-      </Switch>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                {routeList.map((v) => (
+                    <Route key={v.name} exact path={v.path} component={v.page} />
+                ))}
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/components/common/section_button/sectionButtonList.tsx
+++ src/components/common/section_button/sectionButtonList.tsx
@@ -4,7 +4,7 @@ import { sectionRouteList } from "config/routes";
 export function SectionButtonList() {
     return <div className="p-1">
         <div className="w-100 flex justify-center">
-            {sectionRouteList.map((v) => <SectionButton route={v} />)}
+            {sectionRouteList.map((v) => <SectionButton key={v.name} route={v} />)}
         </div>
     </div>;
 }
