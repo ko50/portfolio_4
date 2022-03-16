dde2728345dd24228ef52fcf220ae4df7fad0c9e
ko50 <kotyana1331@gmail.com>
2022-03-17 02:18:25 +0900

    [修正] フィールド名合わせないと

--- src/utils/resourceTypes/works.ts
+++ src/utils/resourceTypes/works.ts
@@ -4,7 +4,7 @@ import { APIResource } from "utils/resourceTypes/base";
 export interface WorkResource extends APIResource {
     name: string;
     description: string;
-    imagePath: string;
+    screenshot: string;
     link: string;
     tags: string[];
 }
