d965220a28e288d2f6c6c2c9ede1cca0fc776179
ko50 <kotyana1331@gmail.com>
2022-03-16 19:51:12 +0900

    [追加] データ取得する処理

--- src/components/common/pageWrapper.tsx
+++ src/components/common/pageWrapper.tsx
@@ -3,6 +3,10 @@ import React from "react";
 import { Background } from "components/common/bg/background";
 import { Header } from "components/common/header/header";
 import { SectionButtonList } from "components/common/section_button/sectionButtonList";
+import { Provider } from "utils/context/context";
+import { APIResource } from "utils/resourceTypes/base";
+import { useLocation } from "react-router-dom";
+import { adapters } from "config/adapters";
 
 
 type _Props = {
@@ -10,10 +14,26 @@ type _Props = {
 }
 
 export function PageWrapper(props: _Props) {
-    return <Background>
-        <Header />
-        <SectionButtonList />
-        <span className="h-4" />
-        <div>{props.children}</div>
-    </Background>;
+    let loading: boolean = true;
+    let data: APIResource[] = [];
+
+    const route: string = useLocation().pathname;
+    if (route !== "/") {
+        const adapter = adapters.get(route)!;
+        console.log(adapter.path);
+
+        adapter.getResources().then((res) => {
+            loading = false;
+            data = res;
+        });
+    }
+
+    return <Provider value={{ loading: loading, data: data }}>
+        <Background>
+            <Header />
+            <SectionButtonList />
+            <span className="h-4" />
+            <div>{props.children}</div>
+        </Background>
+    </Provider>;
 }
